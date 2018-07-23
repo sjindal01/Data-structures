26b2f8ba6e157118df4bfa58566cc55d2b9a00b5
sjindal01 <41490148+sjindal01@users.noreply.github.com>
2018-07-22 21:18:54 -0400

    Add files via upload

---
+++ Deque_Generator.py
@@ -0,0 +1,13 @@
+from Linked_List_Deque import Linked_List_Deque
+from Array_Deque import Array_Deque
+
+
+LL_DEQUE_TYPE = 0
+ARR_DEQUE_TYPE = 1
+
+def get_deque(deque_type=ARR_DEQUE_TYPE):
+  if deque_type == LL_DEQUE_TYPE:
+    return Linked_List_Deque()
+  elif deque_type == ARR_DEQUE_TYPE:
+    return Array_Deque()
+  raise NotImplementedError
